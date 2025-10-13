0abd0a66f9805ce09242f9752facc43cac82ff44
Antonio <antoniofilhojsa@gmail.com>
2025-10-13 20:19:59 -0300

    Spring Boot

---
+++ src/main/java/br/com/unit/modulo_avaliacao_relatorio/ModuloAvaliacaoRelatorioApplication.java
@@ -0,0 +1,13 @@
+package br.com.unit.modulo_avaliacao_relatorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloAvaliacaoRelatorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloAvaliacaoRelatorioApplication.class, args);
+	}
+
+}
