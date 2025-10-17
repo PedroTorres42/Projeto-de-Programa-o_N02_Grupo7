349d53dc8812be7255993ae8142a1b72c2a74736
Antonio <antoniofilhojsa@gmail.com>
2025-10-17 20:21:34 -0300

    Adicionar repositorios

--- src/main/java/br/com/unit/modulo_avaliacao_relatorio/Repositorios/CursoRepositorio.java
+++ src/main/java/br/com/unit/modulo_avaliacao_relatorio/Repositorios/CursoRepositorio.java
@@ -3,5 +3,5 @@ package br.com.unit.modulo_avaliacao_relatorio.Repositorios;
 import br.com.unit.modulo_avaliacao_relatorio.Modelos.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CursoRepositorio extends JpaRepository<Curso, String> {
+public interface CursoRepositorio extends JpaRepository<Curso, Long> {
 }
