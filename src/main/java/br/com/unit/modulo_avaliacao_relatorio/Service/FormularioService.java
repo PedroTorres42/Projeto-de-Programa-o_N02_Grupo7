411c8bd9212f40eea65e3fef6fdc46f081d921e6
Pedro <pedro.tpereira@souunit.com.br>
2025-10-20 19:36:44 -0300

    Criação do FormularioService e UsuarioService

---
+++ src/main/java/br/com/unit/modulo_avaliacao_relatorio/Service/FormularioService.java
@@ -0,0 +1,33 @@
+import br.com.unit.modulo_avaliacao_relatorio.Repositorios.FormularioRepositorio;
+import br.com.unit.modulo_avaliacao_relatorio.Repositorios.PerguntaRepositorio;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class FormularioService {
+
+
+    private final FormularioRepositorio formularioRepositorio;
+    private final PerguntaRepositorio perguntaRepositorio;
+
+
+    public Formulario criarFormulario(String titulo, List<Pergunta> perguntas) {
+        Formulario formulario = new Formulario();
+        formulario.setTitulo(titulo);
+        formulario.setPerguntas(perguntas);
+
+
+        perguntaRepositorio.saveAll(perguntas);
+        return formularioRepositorio.save(formulario);
+    }
+
+
+    public List<Formulario> listarFormularios() {
+        return formularioRepositorio.findAll();
+    }
+}
