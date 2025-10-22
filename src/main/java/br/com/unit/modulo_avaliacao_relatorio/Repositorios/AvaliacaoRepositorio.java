e381c769b29d05ff9d9591cf992629d5a0a11c63
Antonio <antoniofilhojsa@gmail.com>
2025-10-22 16:02:21 -0300

    Adicionar metodos para encontrar curso e instrutor por ID

--- src/main/java/br/com/unit/modulo_avaliacao_relatorio/Repositorios/AvaliacaoRepositorio.java
+++ src/main/java/br/com/unit/modulo_avaliacao_relatorio/Repositorios/AvaliacaoRepositorio.java
@@ -3,4 +3,9 @@ package br.com.unit.modulo_avaliacao_relatorio.Repositorios;
 import br.com.unit.modulo_avaliacao_relatorio.Modelos.Avaliacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AvaliacaoRepositorio extends JpaRepository<Avaliacao, Long> {}
+import java.util.List;
+
+public interface AvaliacaoRepositorio extends JpaRepository<Avaliacao, Long> {
+    List<Avaliacao> findByCursoID(Long id);
+    List<Avaliacao> findByInstrutorID(String id);
+}
