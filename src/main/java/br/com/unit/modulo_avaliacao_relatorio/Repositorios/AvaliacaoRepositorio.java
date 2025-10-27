44e27739fb43c46aaeb0cc750a594ff69a0311f6
Pedro Torres <pedro.tpereira@souunit.com.br>
2025-10-27 19:26:42 -0300

    Criação dos métodos para o AvaliacaoService

--- src/main/java/br/com/unit/modulo_avaliacao_relatorio/Repositorios/AvaliacaoRepositorio.java
+++ src/main/java/br/com/unit/modulo_avaliacao_relatorio/Repositorios/AvaliacaoRepositorio.java
@@ -4,9 +4,13 @@ import br.com.unit.modulo_avaliacao_relatorio.Modelos.Avaliacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AvaliacaoRepositorio extends JpaRepository<Avaliacao, Long> {
     List<Avaliacao> findByCursoId(Long id);
     List<Avaliacao> findByInstrutorId(String id);
+    Optional<Avaliacao> findById(String id);
+    boolean existsById(String id);
+    void deleteById(String id);
 
 }
