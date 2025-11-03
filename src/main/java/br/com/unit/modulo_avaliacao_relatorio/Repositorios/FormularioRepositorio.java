2a66ed3bd182ff6fe66185ff9992c2777faa0dc5
Pedro Torres <pedro.tpereira@souunit.com.br>
2025-11-03 18:49:20 -0300

    Mudança nas regras de negócio de Formulario

--- src/main/java/br/com/unit/modulo_avaliacao_relatorio/Repositorios/FormularioRepositorio.java
+++ src/main/java/br/com/unit/modulo_avaliacao_relatorio/Repositorios/FormularioRepositorio.java
@@ -3,4 +3,8 @@ package br.com.unit.modulo_avaliacao_relatorio.Repositorios;
 import br.com.unit.modulo_avaliacao_relatorio.Modelos.Formulario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FormularioRepositorio extends JpaRepository<Formulario, Long> {}
+import java.util.Optional;
+
+public interface FormularioRepositorio extends JpaRepository<Formulario, Long> {
+	Optional<Formulario> findByTituloAndTipo(String titulo, Formulario.TipoFormulario tipo);
+}
