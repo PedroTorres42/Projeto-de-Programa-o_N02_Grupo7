8bb09802e48e0b305e78d37a7b8718afbf6edd1d
Antonio <antoniofilhojsa@gmail.com>
2025-11-14 14:58:44 -0300

    Remoção de imports não utilizados no FormularioRepositorio

--- src/main/java/br/com/unit/modulo_avaliacao_relatorio/Repositorios/FormularioRepositorio.java
+++ src/main/java/br/com/unit/modulo_avaliacao_relatorio/Repositorios/FormularioRepositorio.java
@@ -2,10 +2,8 @@ package br.com.unit.modulo_avaliacao_relatorio.Repositorios;
 
 import br.com.unit.modulo_avaliacao_relatorio.Modelos.Formulario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+
 import java.util.Optional;
 
 public interface FormularioRepositorio extends JpaRepository<Formulario, Long> {
